e7b9c7365592f6ea8e41c38d87f9558041b6b6a6
sooraj948 <sooraj2.sathish@gmail.com>
2022-02-12 19:28:37 +0530

    going for dinner

--- pcell/views.py
+++ pcell/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from recruiter.models import Recruiter_Post,Skills_Reqd,Eligible_Colleges
 # Create your views here.
 
 def home(request):
     return render(request,"view_posts.html")
 def view_posts(request):
-    return render(request,"view_posts.html")
+    college_name = "IIITB"#change this . Take from logged in name
+    cols = Eligible_Colleges.objects.filter(college_name=college_name)
+    # print(cols)
+    for i in cols:
+        print(i.post_id.company)
+        print(i.college_name)
+    relevant_posts = [i.post_id for i in cols]
+
+    return render(request,"view_posts.html",{"posts":relevant_posts})
