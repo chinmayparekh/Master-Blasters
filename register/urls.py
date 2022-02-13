73d7a12f0650819e2209987baaac5c8d3548bd4e
chinmayparekh <chinmayparekh11@gmail.com>
2022-02-13 10:51:54 +0530

    fixed url routes

--- register/urls.py
+++ register/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='register-home'),
-    path('signUpRecruiter', views.register_rec, name='register-rec'),
-    path('signUpPCell', views.register_cell, name='register-cell'),
-    path('profile',views.profile,name='register-profile'),
+    path('signUpRecruiter/', views.register_rec, name='register-rec'),
+    path('signUpPCell/', views.register_cell, name='register-cell'),
+    path('profile/', views.profile, name='register-profile'),
 ]
-
